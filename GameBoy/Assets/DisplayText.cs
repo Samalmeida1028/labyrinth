46fdeb8494a0d4448632101aea122beaed8b0c8b
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2021-07-31 23:01:49 -0400

    Started making interaction framework

---
+++ GameBoy/Assets/DisplayText.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayText : MonoBehaviour
+{
+
+    public string displayText;
+    public TextMeshProUGUI text;
+    GameObject displayTextOBJ;
+
+    public void Start(){
+    displayText = "Hello";
+    text.GetComponent<TextMeshProUGUI>().text = displayText;
+    text.enabled = false;
+    }
+
+
+public void OnTriggerEnter2D(Collider2D collision){
+    Debug.Log("hi");
+    text.enabled = true;
+    
+
+
+}
+
+}
