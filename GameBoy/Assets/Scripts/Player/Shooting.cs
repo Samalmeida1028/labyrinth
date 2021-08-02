d102417345ae1a972842867be2aa07357693999d
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2021-08-02 10:55:49 -0400

    Update Shooting.cs

--- GameBoy/Assets/Scripts/Player/Shooting.cs
+++ GameBoy/Assets/Scripts/Player/Shooting.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-public Transform firePoint;
+public Transform firePoint;//this is an empty gameobject used for transform
 public GameObject bullet;
 
 public float force =20f;
@@ -21,9 +21,9 @@ public float force =20f;
     }
 
     void Shoot(){
-        GameObject bulletSpawn = Instantiate(bullet, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = bulletSpawn.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up*force, ForceMode2D.Impulse);
+        GameObject bulletSpawn = Instantiate(bullet, firePoint.position, firePoint.rotation);//makes the prefab at the fire point, uses the rotation of the player to determine the shooting angle
+        Rigidbody2D rb = bulletSpawn.GetComponent<Rigidbody2D>();//temp rigidbody for force
+        rb.AddForce(firePoint.up*force, ForceMode2D.Impulse);//applies a force to bullet prefab so it go go (impulse means it is instantaneous not continuous)
 
     }
 }
