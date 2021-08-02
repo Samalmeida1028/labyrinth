3f98f98633e62148b3460c20eb096a828695e8a6
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2021-08-02 10:34:49 -0400

    Update PlayerMovement.cs

--- GameBoy/Assets/Scripts/Player/PlayerMovement.cs
+++ GameBoy/Assets/Scripts/Player/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Update(){
         checkInput();  
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);//conversion to find the location of an object in terms of the camera view, must use this in order for PointToMouse(); to work
     }
 
     private void FixedUpdate(){
@@ -25,13 +25,15 @@ public class PlayerMovement : MonoBehaviour
         void checkInput(){
         float movementX = Input.GetAxisRaw("Horizontal");
         float movementY = Input.GetAxisRaw("Vertical");
-        movement = new Vector2(movementX,movementY).normalized;
+        movement = new Vector2(movementX,movementY).normalized; //normalized just makes the max vector length 1 so diagonal movement isnt faster than vertical or horizontal
     }
 
+
+
      void PointToMouse(){
-        Vector2 lookDir = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg +90f;
-        rb.rotation = angle;
+        Vector2 lookDir = mousePos - rb.position;   //Subtracts both vectors to find the vector pointing towards the mouse (can be used for any object jsut need to get the objects position and convert)
+        float angle = Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg +90f;    //finds angle from horizontal field to the vector pointing toward the mouse (90f just is base rotation you can tweak it)
+        rb.rotation = angle;    //sets look angle to previously found angle
 
     }
 }
