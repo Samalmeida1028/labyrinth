7e58b9b833f0db148c8298e564b1045535e0899a
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2021-08-10 00:40:22 -0400

    Revert "Changed ui to better fit"
    This reverts commit 8d005e3f8273b4e2cbe1cdd350b1aa12950ee6b5.

--- GameBoy/Assets/Scripts/Player/Inventory/InventoryUI.cs
+++ GameBoy/Assets/Scripts/Player/Inventory/InventoryUI.cs
@@ -6,28 +6,27 @@ using UnityEngine;
 public class InventoryUI : MonoBehaviour
 {
     public SpriteRenderer[] display = new SpriteRenderer[4];
-    public GameObject player;
 
     // Start is called before the first frame update
  void Start(){
-     player = GameObject.FindGameObjectWithTag("Player");
+     GameObject player = GameObject.FindGameObjectWithTag("Player");
  }
 
 
 public void AddItemDisplay(Item temp){
-    Debug.Log("adding ui item");
 
+    GameObject player = GameObject.FindGameObjectWithTag("Player");
          display[temp.itemType].GetComponent<SpriteRenderer>().sprite = temp.GetComponent<SpriteRenderer>().sprite;
 }
 
 public void RefreshDisplay(int slot){
     for(int i = 0; i<display.Length; i++){
-        if(display[i].GetComponent<SpriteRenderer>() != null){
         display[i].GetComponent<SpriteRenderer>().color = Color.white;
-        }
+        display[i].GetComponent<Light2D>().intensity = 0;
 
     }
     display[slot].GetComponent<SpriteRenderer>().color = Color.blue;
+    display[slot].GetComponent<Light2D>().intensity = 3;
 
 }
 
