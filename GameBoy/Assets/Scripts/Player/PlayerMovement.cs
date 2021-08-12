70b12d70672495a149e533e2f1322a3a33efb350
Ethan Ferrabelo <74121414+eferrabelo1114@users.noreply.github.com>
2021-08-12 13:16:24 -0400

    New

--- GameBoy/Assets/Scripts/Player/PlayerMovement.cs
+++ GameBoy/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {   
+<<<<<<< Updated upstream:GameBoy/Assets/Scripts/Player/PlayerMovement.cs
  
     Vector2 movement;
     public float counter;
@@ -14,11 +15,32 @@ public class PlayerMovement : MonoBehaviour
 
     public int lookDir;
     public int angle;
+=======
+    //Public Variables
+    public Rigidbody2D RB;
+    public GameObject playerDisplay;
 
+    //Axis Info
+    private float xAxis;
+    private float yAxis;
+>>>>>>> Stashed changes:GameBoy/Assets/Player/PlayerMovement.cs
 
+    //Current
+    private string currentAnimation;
 
+    //Animation States
+    const string PLAYER_IDLE_F = "Idle_Front";
+    const string PLAYER_IDLE_B = "Idle_Back";
 
+    const string PLAYER_WALK_F = "Walk_Forward";
+    const string PLAYER_WALK_B = "Walk_Backward";
 
+    //Other Variables
+    private SpriteRenderer playerSprite;
+    private Animator animator;
+    private Vector2 movement;
+
+<<<<<<< Updated upstream:GameBoy/Assets/Scripts/Player/PlayerMovement.cs
     void Start(){
     }   
      void Update(){
@@ -40,9 +62,20 @@ public class PlayerMovement : MonoBehaviour
         PointToMouse();
 
     }
+=======
+    private bool isAttackPressed;
+    private bool isAttacking;
+>>>>>>> Stashed changes:GameBoy/Assets/Player/PlayerMovement.cs
 
+    private string currentState;
 
+    void Start()    
+    {
+        animator = playerDisplay.GetComponent<Animator>();
+        playerSprite = playerDisplay.GetComponent<SpriteRenderer>();
+    }  
 
+<<<<<<< Updated upstream:GameBoy/Assets/Scripts/Player/PlayerMovement.cs
      void PointToMouse(){
         Vector2 lookDir = mousePos - rb.position;   //Subtracts both vectors to find the vector pointing towards the mouse (can be used for any object jsut need to get the objects position and convert)
         angle = (int)Mathf.Round((Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg - 90f)/45)*45;    //finds angle from horizontal field to the vector pointing toward the mouse (90f just is base rotation you can tweak it)
@@ -51,32 +84,79 @@ public class PlayerMovement : MonoBehaviour
             counter = 0;
         FacingDirection(angle);
         }
+=======
+     void Update()
+     {
+        xAxis = Input.GetAxisRaw("Horizontal");
+        yAxis = Input.GetAxisRaw("Vertical");
+>>>>>>> Stashed changes:GameBoy/Assets/Player/PlayerMovement.cs
 
-
+        //Check for player keyboard input
+        checkInput();  
     }
 
-    void PointInDirection(){
-         if(movement != Vector2.zero)
+    void FixedUpdate()
+    {
+        RB.velocity = movement*GetComponent<PlayerStats>().moveSpeed;
+    }
+    
+    void checkInput()
+    {
+        // Movement
+        movement = new Vector2(xAxis,yAxis).normalized; 
+
+        //Check what axis the player is pointing towards
+        if (xAxis < 0 ) //Moving Left
         {
-            Quaternion toRotate = Quaternion.LookRotation(Vector3.forward, movement);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation,toRotate,rotationSpeed*Time.deltaTime);
+            if (yAxis > 0)//Moving Up
+            {
+            playerSprite.flipX = true;
+            ChangeAnimationState(PLAYER_WALK_B);
+            }
+            else
+            {
+            playerSprite.flipX = true;
+            ChangeAnimationState(PLAYER_WALK_F);
+            }
+
+        }
+        else if (xAxis >0 ) //Moving Right
+        {
+            if (yAxis > 0)//Moving Up
+            {
+            playerSprite.flipX = false;
+            ChangeAnimationState(PLAYER_WALK_B);
+            }
+            else
+            {
+            playerSprite.flipX = false;
+            ChangeAnimationState(PLAYER_WALK_F);
+            }
+        }
+        else if (yAxis > 0) //Moving Up
+        {
+            playerSprite.flipX = false;
+            ChangeAnimationState(PLAYER_WALK_B);
+        }
+        else if (yAxis < 0) //Moving Down
+        {
+            playerSprite.flipX = false;
+            ChangeAnimationState(PLAYER_WALK_F);
+        }
+        else //Not Moving
+        {
+            ChangeAnimationState(PLAYER_IDLE_F);
         }
     }
 
-    void FacingDirection(float angle){
-        if(angle==0){
-            lookDir = 0; 
-        }
-        if(angle == -180){
-            lookDir = 1;
-        }
-        if(angle == -90){
-            lookDir = 2;
-        }
-        if(angle == 90){
-            lookDir = 3;
-        }
+    void ChangeAnimationState(string newState)
+    {
+        //Stop the same animation from fucking itself
+        if (currentState == newState) return;
 
-
+        //pLAY THAT MF
+        animator.Play(newState);
     }
+
+
 }
