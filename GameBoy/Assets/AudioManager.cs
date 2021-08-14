fd7a04b9dd32e3061853abe203fda3084ca5ce54
Ethan Ferrabelo <74121414+eferrabelo1114@users.noreply.github.com>
2021-08-14 00:43:27 -0400

    Audio Testing

--- GameBoy/Assets/AudioManager.cs
+++ GameBoy/Assets/AudioManager.cs
@@ -7,11 +7,17 @@ public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
 
+    void Start()
+    {
+        Play("Title_Music");
+    }
+
     void Awake() {
         foreach (Sound s in sounds) {
             s.source=gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
-
+            
+            s.source.loop = s.loop;
             s.source.volume = s.volume;
 
             s.source.pitch = s.pitch;
@@ -19,9 +25,9 @@ public class AudioManager : MonoBehaviour
     }
 
     public void Play(string name) {
-
         Sound s = Array.Find(sounds, sound => sound.name == name);
         if (s == null) {
+            Debug.Log("Couldn't find audio!");
             return;
         }
         s.source.Play();
